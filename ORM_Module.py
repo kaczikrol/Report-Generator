9c38c36206a16248094e9418d23b3cfafac24e9d
Przemysław Kaczmarek <Przemyslaw.Michal.Kaczmarek@gmail.com>
2017-12-12 21:09:27 +0100

    Report Generator in progress

--- ORM_Module.py
+++ ORM_Module.py
@@ -4,18 +4,35 @@ from sqlalchemy.orm import sessionmaker
 
 #it's important to add in SQL Server new database: ReportGenerator
 
-class Connection():
+"""
+class Connection(sessionmaker):
     __Server='DOM'
     __Database='ReportGenerator'
     __Driver='SQL Server Native Client 11.0'
-    __Engine = create_engine("mssql+pyodbc://" + __Server + "/" + __Database + "?driver=" + __Driver)
-    Base = declarative_base(bind=__Engine)
+    Engine = create_engine("mssql+pyodbc://" + __Server + "/" + __Database + "?driver=" + __Driver)
+    Base = declarative_base(bind=Engine)
     Meta = Base.metadata
-    session = sessionmaker(bind=__Engine)
+    session = sessionmaker(bind=Engine)
     Session = session()
 
+    def __init__(self):
+        super().__init__(self)
 
-class User(Connection.Base):
+    def add(self,**kwargs):
+        super(Connection,self).add()
+
+    def commit(self):
+        super(Connection,self).commit()
+"""
+
+Engine=create_engine("mssql+pyodbc://DOM/ReportGenerator?driver=SQL Server Native Client 11.0")
+Base = declarative_base(bind=Engine)
+Meta = Base.metadata
+Session = sessionmaker(bind=Engine)
+session = Session()
+
+
+class User(Base):
     __tablename__ = 'Users'
     Id = Column(Integer, primary_key=True)
     FirstName = Column(NVARCHAR(15))
@@ -33,7 +50,7 @@ class User(Connection.Base):
         return (self.FirstName,self.LastName,self.City,self.Country)
 
 
-class Operation(Connection.Base):
+class Operation(Base):
     __tablename__ = 'Operations'
     Id = Column(Integer, primary_key=True)
     User_Id = Column(Integer, ForeignKey('Users.Id'))
@@ -52,4 +69,6 @@ class Operation(Connection.Base):
 
     def __repr__(self):
         return (self.Date,self.Type,self.Value)
+
+
 
--- main.py
+++ main.py
@@ -1,6 +1,31 @@
+from PyQt4 import QtCore, QtGui
+from Design import Ui_MainWindow
+import sys
 import ORM_Module
-import Design
 
-def main():
-    pass
-main()
+class MyForm(QtGui.QMainWindow):
+    def __init__(self, parent=None):
+        QtGui.QWidget.__init__(self, parent)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+        QtCore.QObject.connect(self.ui.ExpenseButton,QtCore.SIGNAL("clicked()"),self.addExpense)
+        QtCore.QObject.connect(self.ui.IncomeButton,QtCore.SIGNAL("clicked()"),self.addIncome)
+
+
+    def addExpense(self):
+        self.expense=ORM_Module.Operation('2017-12-12','C',124.12)
+        ORM_Module.session.add(self.expense)
+        ORM_Module.session.commit()
+
+    def addIncome(self):
+        self.income=ORM_Module.Operation('2017-12-12','D',112.23)
+        ORM_Module.session.add(self.income)
+        ORM_Module.session.commit()
+
+
+if __name__ == "__main__":
+    app = QtGui.QApplication(sys.argv)
+    myapp = MyForm()
+    myapp.show()
+    sys.exit(app.exec_())
